01454166702693e339042cd112f4aeba02d4311a
Atakan Cengiz KURT <atakan.kurt@loodos.com>
2021-05-17 09:43:32 +0300

    Initial Commit

---
+++ SampleAnimation-SwiftUI/SampleAnimation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAnimation_SwiftUIApp.swift
+//  SampleAnimation-SwiftUI
+//
+//  Created by Atakan Cengiz KURT on 17.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct SampleAnimation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
